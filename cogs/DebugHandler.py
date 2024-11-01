b31df97c1eefcc7d44ca2f7fe1a65b23d0941db5
MooreGaming1324 <mooreboys742@gmail.com>
2024-11-01 13:10:42 -0400

    formatting and ignore pycache

--- cogs/DebugHandler.py
+++ cogs/DebugHandler.py
@@ -6,13 +6,16 @@ class DebugHandler(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    #If user is owner check
+    async def is_owner(ctx: commands.Context):
+        return ctx.author.id == 560123152349528066
+
+    #Notify if cog is working
     @commands.Cog.listener()
     async def on_ready(self):
         print("DebugHandler is active")
 
-    ## Sync command for only owner
-    async def is_owner(ctx: commands.Context):
-        return ctx.author.id == 560123152349528066
+    #Sync app commands, owner only
     @commands.command()
     @commands.check(is_owner)
     async def sync(self, ctx: commands.Context):
@@ -23,15 +26,14 @@ class DebugHandler(commands.Cog):
         except Exception as e:
             await message.edit(content=("An error has occurred with syncing: ", e))
 
+    #Ping application command
     @app_commands.command(name="ping",description="Gets the bot's latency")
     async def ping(self, interaction: discord.Interaction):
-        ping_embed = discord.Embed(title="Pong!", color=discord.Color.random())
+        ping_embed = discord.Embed(title="Pong!", color=discord.Color.green())
         ping_embed.set_footer(text=f"Requested by {interaction.user.name}",icon_url=interaction.user.avatar)
         ping_embed.add_field(name=f"Bot's Latency: {round(self.bot.latency * 1000)}ms",value="")
         await interaction.response.send_message(embed=ping_embed)
 
     
-
-
 async def setup(bot):
     await bot.add_cog(DebugHandler(bot))
--- main.py
+++ main.py
@@ -4,35 +4,29 @@ import discord
 from discord.ext import commands
 import asyncio
 
+#Imports bot token from .env
 load_dotenv()
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
+#Creates bot with set intents
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=".",intents=intents)
 
-
+#Notify for bot status
 @bot.event
 async def on_ready():
     print(f'{bot.user} is online!')
 
-
-
-@bot.tree.command(name="hello",description="Says hello")
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f'Hello!')
-
-
-
-
+#Load cogs
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
+#Start cogs and bot
 async def main():
     async with bot:
         await load()
         await bot.start(TOKEN)
-
 asyncio.run(main())
